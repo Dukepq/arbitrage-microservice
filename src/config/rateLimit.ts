61c6b73986cad3893f8e0cb1626054265b9a0bc6
Dukepq <maikelvdj@live.be>
2024-03-08 19:37:02 +0100

    chore: lower rate limit

--- src/config/rateLimit.ts
+++ src/config/rateLimit.ts
@@ -2,7 +2,7 @@ import { rateLimit } from "express-rate-limit";
 
 const limiter = rateLimit({
   windowMs: 60 * 1000,
-  limit: 120,
+  limit: 60,
   standardHeaders: "draft-6",
   legacyHeaders: false,
 });
